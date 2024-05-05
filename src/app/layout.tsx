f9df0518c868aedf0078695e636cff5ef3099a60
Elke Harmanny <ElkeHarmanny@outlook.com>
2024-05-05 15:36:38 +0200

    Move banner to background image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import MainMenu from "@/ui/MainMenu";
+import Image from "next/image";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,10 +19,19 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${inter.className} bg-slate-100 flex h-screen flex-col items-center px-24 py-16 `}
+        className={`${inter.className} h-screen w-screen flex justify-center`}
       >
-        <MainMenu />
-        {children}
+        <Image
+          src={"/banner.jpg"}
+          alt={"Banner straatwerk"}
+          fill={true}
+          className="cover-object z-0 opacity-50"
+        />
+
+        <div className="flex flex-col items-center bg-slate-100 z-10 w-[75vw] mt-20 rounded opacity-80">
+          <MainMenu />
+          {children}
+        </div>
       </body>
     </html>
   );
--- src/ui/MainMenu.tsx
+++ src/ui/MainMenu.tsx
@@ -1,27 +1,11 @@
-import Image from "next/image";
-import banner from "../../public/banner.png";
 import MenuItem from "./MenuItem";
 
 export default function MainMenu() {
   return (
-    <div className="relative flex items-center">
-      <div className="z-10 absolute top-5 w-full">
-        <div className="flex flex-row space-x-10 justify-center w-full ">
-          <MenuItem name="Home" link="/" />
-          <MenuItem name="Portfolio" link="/Portfolio" />
-          <MenuItem name="Contact" link="/Contact" />
-        </div>
-      </div>
-
-      <Image
-        src={banner}
-        alt={"Banner straatwerk"}
-        style={{
-          width: "100%",
-          height: "auto",
-        }}
-        className="z-0"
-      />
+    <div className="flex space-x-10 justify-center w-full mt-10">
+      <MenuItem name="Home" link="/" />
+      <MenuItem name="Portfolio" link="/Portfolio" />
+      <MenuItem name="Contact" link="/Contact" />
     </div>
   );
 }
