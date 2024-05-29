713d8663e58ef0d4f96c2c5e40baae72c9c7469c
Elke Harmanny <ElkeHarmanny@outlook.com>
2024-05-29 16:59:55 +0200

    Make image path depend on env

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,7 @@ export default function Home() {
       <h1 className="text-xl">Welkom op de site van Meester Bestrating</h1>
       <h5>Uw specialist in (sier)bestrating.</h5>
       <img
-        src="./public/images/portfolio/project-8.jpeg"
+        src={`./${import.meta.env.PROD ? "" : "public/"}images/portfolio/project-8.jpeg`}
         className="max-w-[50vw] mt-10"
       ></img>
     </>
--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -1,3 +1,12 @@
 export default function Portfolio() {
-  return <div>Portfolio Pagina</div>;
+  // Create array of 18 elements
+  // Loop through, and create a thumbnail/preview for each
+  // When clicked on, takes you to seperate page that will show other photo's that belong to
+  // that project
+
+  return (
+    <>
+      <div>Een greep uit de eerder uitgevoerde projecten:</div>;{}
+    </>
+  );
 }
