74f6cdea2c8c06510a0cc0059ea1ca63248167fb
Elke Harmanny <ElkeHarmanny@outlook.com>
2024-05-29 17:08:31 +0200

    Fix frontpage overflow

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,11 @@
 export default function Home() {
   return (
     <>
-      <h1 className="text-xl">Welkom op de site van Meester Bestrating</h1>
+      <h1 className="text-xl">Welkom bij Meester Bestrating</h1>
       <h5>Uw specialist in (sier)bestrating.</h5>
       <img
         src={`./${import.meta.env.PROD ? "" : "public/"}images/portfolio/project-8.jpeg`}
-        className="max-w-[50vw] mt-10"
+        className="max-w-[45vw] mt-10"
       ></img>
     </>
   );
