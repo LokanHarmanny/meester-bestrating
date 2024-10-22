b01c588f95f89841d395151f7f3da69c5a5288a3
Lokan <Lokanharmanny@outlook.com>
2024-10-22 21:48:20 +0200

    Fix pictures not displaying

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { resizeImage } from "../helpers";
 import PortfolioImage from "../ui/PortfolioImage";
 
@@ -6,18 +7,24 @@ export default function Portfolio() {
   // Loop through, and create a thumbnail/preview for each
   // When clicked on, takes you to seperate page that will show other photo's that belong to
   // that project
+
+  return (
+    <>
+      <div>Een greep uit de eerder uitgevoerde projecten:</div>
+      <div className="flex max-w-[50vw] gap-8 flex-wrap">{generatePictureArray(19)}</div>
+    </>
+  );
+}
+
+function generatePictureArray(amount: number) {
   const pictureArray = [];
-  for (let i = 1; i < 19; i++) {
+  
+  for (let i = 1; i < amount; i++) {
     const image = new Image();
     image.src = `./images/portfolio/project-${i}.jpeg`;
     const imageUrl = resizeImage(image).toDataURL("image/jpeg", 1.0);
     pictureArray.push(<img src={imageUrl} key={`image-${i}`} />);
   }
 
-  return (
-    <>
-      <div>Een greep uit de eerder uitgevoerde projecten:</div>
-      <div className="flex max-w-[50vw] gap-8 flex-wrap">{...pictureArray}</div>
-    </>
-  );
+  return pictureArray
 }
