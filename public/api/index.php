61a4e44db29a84298b1cc84e22339c1a1606eefb
Lokan <Lokanharmanny@outlook.com>
2024-10-23 21:43:43 +0200

    Implement actual returning of files from API endpoint

--- public/api/index.php
+++ public/api/index.php
@@ -3,18 +3,40 @@ header("Content-Type: application/json");
 header("Access-Control-Allow-Origin: *");
 
 $requestMethod = $_SERVER["REQUEST_METHOD"];
+$action = $_GET['action'] ?? ';';
 
-switch($requestMethod) {
-    case 'GET':
-        handleGetRequest();
-        break;
-    default:
-        echo json_encode(["message" => "Request method not supported"]);
-        break;
+if ($requestMethod === 'GET') {
+    switch ($action) {
+        case 'getPortfolio':
+            getPortfolio();
+            break;
+        default:
+            echo json_encode(["message" => "Request action not recognized"]);
+            break;
+    }
+} else {
+    echo json_encode(["message" => "Request method not supported"]);
 }
 
-function handleGetRequest() {
-    echo "This is a GET request";
+function getPortfolio() {
+    $directoryPath = '../images/portfolio';
+    $filesArray = getFiles($directoryPath);
+    echo json_encode($filesArray);
+}
+
+function getFiles($dir) {
+    $files = [];
+    if (is_dir($dir)) {
+        if ($dh = opendir($dir)) {
+            while (($file = readdir($dh)) !== false) {
+                if ($file != '.' && $file != '..') {
+                    $files[] = $file;
+                }
+            }
+            closedir($dh);
+        }
+    }
+    return $files;
 }
 
 ?>
