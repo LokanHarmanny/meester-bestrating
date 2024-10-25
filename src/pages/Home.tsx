40a998b4bc21b3c23949a3b0eae6ec5f7d7a8053
Lokan <Lokanharmanny@outlook.com>
2024-10-25 20:09:37 +0200

    Edit home banner photo to emphasize the pavement

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,12 @@
 export default function Home() {
-  return (
-    <>
-      <h1 className="text-xl">Welkom bij Meester Bestrating</h1>
-      <h5>Uw specialist in (sier)bestrating.</h5>
-      <img
-        src={`./images/portfolio/project-8.jpeg`}
-        className="max-w-[45vw] mt-10"
-      ></img>
-    </>
-  );
+	return (
+		<>
+			<h1 className="text-xl">Welkom bij Meester Bestrating</h1>
+			<h5>Uw specialist in (sier)bestrating.</h5>
+			<img
+				src={`./images/home_banner.jpeg`}
+				className="max-w-[45vw] mt-10"
+			></img>
+		</>
+	)
 }
